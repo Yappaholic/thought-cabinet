71eda32d2d741e03a536c02190c4f5c54fb7ce88
yappaholic <sav.boyar@gmail.com>
2024-12-06 20:28:18 +0300

    Feat: made the main part of the page

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Main from './Main';
+import "./index.css"
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <Main />
     </React.StrictMode>,
   );
 }
